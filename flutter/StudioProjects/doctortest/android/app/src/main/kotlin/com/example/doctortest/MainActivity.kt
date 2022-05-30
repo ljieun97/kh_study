224c08b3297a6af312620ea9bb88564241f92aae
ljieun <wldms8196@gmail.com>
2022-05-30 18:05:13 +0900

    flutter first

---
+++ flutter/StudioProjects/doctortest/android/app/src/main/kotlin/com/example/doctortest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.doctortest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/StudioProjects/first/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:first/arithfunc.dart';
+import 'package:first/type_estimate.dart';
+import 'package:first/valfunc.dart';
+import 'package:first/vartypefunc.dart';
+
+void main() {
+  varfunc_test();
+  vartypefunc_test();
+  type_estimate_func_test();
+  airth_func_test();
+}
+
